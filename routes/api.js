40fa6ce57530988cbb8057a72a6bc90eb3448fb2
Brandon Haley <kyle7286@gmail.com>
2021-04-21 18:39:37 -0400

    completed basic routing

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+// const router = require("express").Router();
+// const Workout = require("../models/workout.js");
+
+
+// // New Exerice
+// router.post("/api/transaction", ({ body }, res) => {
+//     Transaction.create(body)
+//       .then(dbTransaction => {
+//         res.json(dbTransaction);
+//       })
+//       .catch(err => {
+//         res.status(400).json(err);
+//       });
+//   });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const router = require("express").Router();
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,9 +18,14 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api.js"));
+// Exercise Page | http://localhost:3000/exercise
+app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, './public/exercise.html')));
+
+// Stats Page | http://localhost:3000/stats
+app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, './public/stats.html')));
+
 
 app.listen(PORT, () => {
-  console.log(`App running on port https://localhost:${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}!`);
 });
+
