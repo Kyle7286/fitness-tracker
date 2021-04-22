d9cf500f732c29ae0e38e9ea3938a94a09a5ab63
Brandon Haley <kyle7286@gmail.com>
2021-04-21 20:01:40 -0400

    Completed add exercise api route

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const Workout = require("../models/Workout.js");
 
 
 
-// getLastWorkout() | /api/workouts
+// Displays | /api/workouts
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
         .then(data => {
@@ -15,20 +15,31 @@ router.get("/api/workouts", (req, res) => {
 });
 
 // Add Exercise | /api/workouts/608096aa6918b331584a17f8
-router.post("/api/workouts/:id", (req, res) => {
-    
-    console.log(req.params.id);
-    res.json(req.body)
-    
-    // Workout.create(body)
-    //   .then(data => {
-    //     res.json(data);
-    //   })
-    //   .catch(err => {
-    //     res.status(400).json(err);
-    //   });
+router.put("/api/workouts/:id", (req, res) => {
 
-  });
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+        .then(data => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+// Workout Exercise | /api/workouts/608096aa6918b331584a17f8
+router.put("/api/workouts/", ({ body }, res) => {
+
+    Workout.create(body)
+        .then(data => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 // Export routes for use in server?
 module.exports = router;
